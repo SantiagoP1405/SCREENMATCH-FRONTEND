71e25cbc287a3804d791111e052e26960f4a6883
Santiago Gómez <santipatito04@gmail.com>
2025-01-12 11:11:58 -0600

    Versión 5 - Se inyectaron las dependencias del repositorio - Se modificó el método para mostrar un mensaje en el localhost, para que ahora muestre una lista de series (las que ya se tenían en el repositorio)

--- java-spring-2-main/src/main/java/com/aluracursos/santiagogomez/screenmatch_spring/controller/SerieController.java
+++ java-spring-2-main/src/main/java/com/aluracursos/santiagogomez/screenmatch_spring/controller/SerieController.java
@@ -1,14 +1,22 @@
 package com.aluracursos.santiagogomez.screenmatch_spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aluracursos.santiagogomez.screenmatch_spring.model.Serie;
+import com.aluracursos.santiagogomez.screenmatch_spring.respository.SerieRepository;
+
 @RestController //Este controlador manejará solicitudes HTTP y devolverá respuestas en formato JSON
 public class SerieController {
+    @Autowired
+    private SerieRepository repository;
 
-    @GetMapping("/series")
-    public String mostrarMensaje(){
-        return "primer mensaje ☺☻♥♦";
-    } //para mapear solicitudes HTTP GET a un método específico en un controlador
+    @GetMapping("/series")//para mapear solicitudes HTTP GET a un método específico en un controlador
+    public List<Serie> obtenerSeries(){
+        return repository.findAll();
+    } 
 
 }

ea816fe35a406154bf56bfc346a28fed2f6237ab
Santiago Gómez <santipatito04@gmail.com>
2025-01-12 10:51:36 -0600

    Versión 3 Continuación del proyecto Screenmatch para el curso de "Java: creando tu primera API y conectándola a Front End" - Se añadieron las dependencias de springboot web - Se copió el archivo ejecutable para no perder el archivo original de la consola, y poder modificarlo - Se eliminó la Interfaz de command line runner del archivo ejecutable - Se eliminó la inyección de dependencias de repository en el archivo ejecutable

--- java-spring-2-main/src/main/java/com/aluracursos/santiagogomez/screenmatch_spring/ScreenmatchSpringApplicationConsola.java
+++ java-spring-2-main/src/main/java/com/aluracursos/santiagogomez/screenmatch_spring/ScreenmatchSpringApplicationConsola.java
@@ -17,10 +17,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-public class ScreenmatchSpringApplication{
-
+public class ScreenmatchSpringApplicationConsola implements CommandLineRunner {
+	@Autowired // se utiliza para realizar la inyección de dependencias. Esto significa que le estás diciendo a Spring que quieres que maneje la creación e inyección de una instancia de una clase en otra clase.
+	private SerieRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.muestraMenu();
+
+		/*EjemploStreams ejemploStreams = new EjemploStreams();
+		ejemploStreams.muestraEjemplo();*/
 	}
 }
 
